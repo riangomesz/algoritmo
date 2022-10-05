b86da890c07e26c5106bca93efed8cd1b49fb20f
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-05 14:03:34 -0300

    Create atvd3.js
    Atividade de algoritmo 3 "média, soma e multiplicação de notas"

---
+++ códigos para o Git/atvd3.js
@@ -0,0 +1,19 @@
+"Algoritmo para saber a soma, media e a multiplicacao de algumas de 2 notas " 
+
+var nota1, nota2 , media, real;
+inicio
+// Secao de Comandos
+  Escreva ("Digite nota1");
+  leia (nota1);
+  
+  Escreva ("Digite nota2");
+  leia (nota2);
+
+  media = (nota1 + nota2) / 2;
+  soma = nota1 + nota2;
+  multiplicacao= nota1*nota2;
+
+  Escreva ("A media e: "+media);
+  Escreva ("A soma e: "+soma);
+  Escreva ("A multiplicacao e: "+multiplicacao);
+fimalgoritmo
