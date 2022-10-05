312bbaf440499820316b51d39734ea3450b23da2
Rian Oliveira Rocha Gomes <rian.oliveira@AMERICA.INT>
2022-10-05 12:48:06 -0300

    algortimo

---
+++ códigos para o Git/atividade.js
@@ -0,0 +1,27 @@
+/*
+ 
+ Olá, estarei compartilhando com vocês algumas atividades feitas por mim
+ utilizando algoritmos
+
+ Algoritmo "Média, Soma e Multiplicação da nota dos Alunos"
+
+ var 
+ int 
+ nota1, nota2 , media, real;
+ início
+
+ // Seção de Comandos
+ Escreval ("Digite nota1");
+ leia (nota1);
+
+ Escreval ("Digite nota2");
+ leia (nota2);
+
+ media = nota1+nota2/2;
+ soma = nota1+nota2;
+ multiplicação = nota1*nota2;
+
+ fimalgoritmo
+
+
+ */
