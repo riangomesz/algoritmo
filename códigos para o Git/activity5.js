32aab76a6aa105af2c5a245543c47f2b0ddde7ed
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-05 14:18:06 -0300

    Create activity5.js
    soma de 3 variáveis diferentes utilizando portugol

---
+++ códigos para o Git/activity5.js
@@ -0,0 +1,22 @@
+Algoritmo "leitura de 3 números e calcule Soma"
+Declare A, B, C, soma1, soma2, soma3: numérico;
+
+// Seção de Comandos
+  Escreva("A");
+  leia(A);
+
+  Escreva("B");
+  leia(B);
+
+  Escreva("C");
+  leia(C);
+  
+  soma1=A+B
+  soma2=B+C
+  soma3=C+A
+
+  Escreva ("A soma de A+B é: ,"soma1);
+  Escreva ("A soma de B+C é: ,"soma2);
+  Escreva ("A soma de C+A é: ,"soma3);
+
+fimalgoritmo
