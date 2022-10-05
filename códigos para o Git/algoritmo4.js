121b95a98df5eb33e891127a4bdfdbe465521e4c
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-05 14:14:58 -0300

    Create algoritmo4.js
    Diferença do maior para o menor número, atividade com algoritmo

---
+++ códigos para o Git/algoritmo4.js
@@ -0,0 +1,18 @@
+Algoritmo:" Leitura de 2 números e a diferença entre o maior pelo menor"
+
+Declare A, B: Inteiro;
+início
+
+// Seção de Comandos 
+  Escreva("Informe o primeiro valor: ");
+  Leia(A);
+ 
+  Escreva("Informe o segundo valor: ");
+  Leia(B);
+ 
+  se (A > B) entao
+    Escreva("A diferença entre o números lidos é ", (A - B));
+  senao
+    Escreva("A diferença entre o números lidos é ", (B - A));
+
+fimalgoritmo
